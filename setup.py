439b1d7e2f0aaafe93d7dd60c0b5e67c2bf9e191
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-08-08 16:53:25 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from cx_Freeze import setup, Executable
 
-setup(name="QR Screenshot Scanner", executables=[Executable("QR Screenshot Scanner.py")], options={"build_exe": {"excludes": ["tkinter"]}})
+setup(name="QR Screenshot Scanner", executables=[Executable("QR Screenshot Scanner.py")])
